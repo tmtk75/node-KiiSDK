b8e2fdbcd09fc845697bd12e9e38fec2d5fae38d
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-09-11 13:57:10 +0900

    initial import

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+require("node-jquery-xhr");
+var KiiSDK = require("./KiiSDK-2.1.20.js");
+var root = KiiSDK.create();
+var clsz = KiiSDK.exportedClasses;
+for (var i = 0; i < clsz.length; i++) {
+  var n = clsz[i];
+  eval(n + " = root." + n);
+}
+
