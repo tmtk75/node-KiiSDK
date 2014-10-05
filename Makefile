63602dab3a55b384a090c434f3f33f7fde0ad41c
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-10-05 11:20:12 +0900

    minify

---
+++ Makefile
@@ -0,0 +1,3 @@
+KiiSDK-2.1.20.min.js: KiiSDK-2.1.20.js
+	./node_modules/.bin/minify KiiSDK-2.1.20.js > KiiSDK-2.1.20.min.js
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 require("node-jquery-xhr");
-var KiiSDK = require("./KiiSDK-2.1.20.js");
+var KiiSDK = require("./KiiSDK-2.1.20.min.js");
 var root = KiiSDK.create();
 var clsz = KiiSDK.exportedClasses;
 for (var i = 0; i < clsz.length; i++) {
